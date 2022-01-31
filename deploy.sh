580b14a0f8cb5c4c8b76b068156fde929ac3d1c5
karasyuk <karasyuk@promsoft.ru>
2022-01-31 22:12:10 +0700

    ci/cd

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 cd ~/COVID-19 && git pull
 cd ~/covid19-prediction && git pull
-source covid19-prediction/venv/bin/activate
+source venv/bin/activate
 pip-sync
-echo 'ACCESS_KEY_ID=${{ secrets.ACCESS_KEY_ID }}' > covid19-prediction/.env
-echo 'SECRET_ACCESS_KEY_ID=${{ secrets.SECRET_ACCESS_KEY_ID }}' >> covid19-prediction/.env
+echo 'ACCESS_KEY_ID=${{ secrets.ACCESS_KEY_ID }}' > .env
+echo 'SECRET_ACCESS_KEY_ID=${{ secrets.SECRET_ACCESS_KEY_ID }}' >> .env
 doit
