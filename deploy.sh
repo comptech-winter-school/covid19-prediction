b01d62b22522066149dceb02e809f1d6278ea4c6
karasyuk <karasyuk@promsoft.ru>
2022-01-31 21:37:38 +0700

    ci/cd

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,3 @@ pip-sync
 echo 'ACCESS_KEY_ID=${{ secrets.ACCESS_KEY_ID }}' > covid19-prediction/.env
 echo 'SECRET_ACCESS_KEY_ID=${{ secrets.SECRET_ACCESS_KEY_ID }}' >> covid19-prediction/.env
 doit
-deactivate
