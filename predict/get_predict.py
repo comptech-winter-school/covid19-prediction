87d92a723ef60cd2df75ab06b5de78df9c007cc4
karasyuk <karasyuk@promsoft.ru>
2022-02-05 11:50:42 +0700

    predict

--- predict/get_predict.py
+++ predict/get_predict.py
@@ -8,10 +8,12 @@ from datetime import date, timedelta, datetime
 from statsmodels.tsa.holtwinters import ExponentialSmoothing as HWES
 import statsmodels.api as sm
 
+
 def get_cases(days):
-  data = httpx.get(f"https://storage.yandexcloud.net/covid-19/cases{\
-      ('_' + str(days)) if days != 0 else ''}.pickle").content
-  return pickle.loads(data)
+    data = httpx.get(
+        f"https://storage.yandexcloud.net/covid-19/cases{('_' + str(days)) if days != 0 else ''}.pickle").content
+    return pickle.loads(data)
+
 
 # ================================PROPHET================================
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
@@ -71,27 +73,29 @@ def get_predict(days_predict, source_country, cases, lags) -> np.array:
     future = get_df_future(days_predict, m, countries, cases, field, train.index[-1])
     forecast = m.predict(future)
     return forecast[["ds", 'yhat']]
+
+
 # ///////////////////////////////////////////////////////////////////////
 # =======================================================================
 
 
 # =================================MEAN==================================
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-  
-def get_predict_mean(country, n_days, need_predict, cases_all, use_filter=True, pred_from=None):
 
+def get_predict_mean(country, n_days, need_predict, cases_all, use_filter=True, pred_from=None):
     field_name = ("filtered_" if use_filter else "") + "history"
     threshold = date(year=2021, month=1, day=1)
 
     just_cases = cases_all[country][field_name]
     if pred_from:
-        mean_value = np.mean(just_cases[pred_from - n_days : pred_from].values)
+        mean_value = np.mean(just_cases[pred_from - n_days: pred_from].values)
     else:
-        mean_value = np.mean(just_cases[- n_days : ].values)
+        mean_value = np.mean(just_cases[- n_days:].values)
 
     predict = np.asarray([mean_value for i in range(0, need_predict)])
 
     return predict
+
 
 # ///////////////////////////////////////////////////////////////////////
 # =======================================================================
@@ -100,71 +104,71 @@ def get_predict_mean(country, n_days, need_predict, cases_all, use_filter=True, 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 def get_columns(country, top_3_countries, num_features):
-  first = [f"{country}_{i}" for i in range(1, 31)]
-  second = []
-  for c, _ in top_3_countries:
-    for i in range(1, num_features + 1):  
-      second.append(f"{c}_{i}")
-  second.append('y')
-  return first + second
+    first = [f"{country}_{i}" for i in range(1, 31)]
+    second = []
+    for c, _ in top_3_countries:
+        for i in range(1, num_features + 1):
+            second.append(f"{c}_{i}")
+    second.append('y')
+    return first + second
 
 
 def get_predict_linear(country, lags, need_predict, cases_all, use_filter=True, num_features=30):
+    field_name = ("filtered_" if use_filter else "") + "history"
+    threshold = date(year=2021, month=1, day=1)
 
-  field_name = ("filtered_" if use_filter else "") + "history"
-  threshold = date(year=2021, month=1, day=1)
+    # PREPARE TRAIN DATA
+    a = cases_all[country][field_name]  # случаи по стране cглаженные или нет
+    a = a[a.index >= threshold]  # отсечение - берем даты после 01.01.2021
+    df_list = []
+    top_3_countries = [
+                          i for i in sorted(lags[country].items(),
+                                            key=lambda x: x[1]["similarity"]
+                                            ) if -i[1]["lag"] > need_predict
+                      ][-3:]  # топ-3 страны с максимальной корреляцией
 
-  # PREPARE TRAIN DATA
-  a = cases_all[country][field_name] #случаи по стране cглаженные или нет
-  a = a[a.index >= threshold] #отсечение - берем даты после 01.01.2021
-  df_list = []
-  top_3_countries = [
-    i for i in sorted(lags[country].items(),
-                        key=lambda x: x[1]["similarity"]
-                      ) if -i[1]["lag"] > need_predict
-    ][-3:] #топ-3 страны с максимальной корреляцией
+    for window in range(num_features, (
+            a.index[-1] - threshold).days):  # window проходит значения от кол-ва пр-ков до кол-ва дней в рассм. cases
+        this_row = list(a[window - num_features: window].values)
+        y = a[window]
 
-  for window in range(num_features, (a.index[-1] - threshold).days): # window проходит значения от кол-ва пр-ков до кол-ва дней в рассм. cases
-    this_row = list(a[window - num_features : window].values)
-    y = a[window]
+        for country_similar, info in top_3_countries:
+            b = cases_all[country_similar][field_name]
+            this_row += list(b[b.index > threshold - timedelta(days=-info["lag"])]
+                             [window - num_features: window].values)
 
-    for country_similar, info in top_3_countries:
+        this_row.append(y)
+        df_list.append(this_row)
 
-      b = cases_all[country_similar][field_name]
-      this_row += list(b[b.index > threshold - timedelta(days=-info["lag"])]
-                       [window - num_features : window].values)
+    df = pd.DataFrame(df_list, columns=get_columns(country, top_3_countries, num_features))
 
-    this_row.append(y)
-    df_list.append(this_row)
+    # FIT
 
-  df = pd.DataFrame(df_list, columns=get_columns(country, top_3_countries, num_features))
+    X = sm.add_constant(df.drop('y', axis=1))
+    y = df['y']
+    model = sm.OLS(y, X).fit()
 
-  # FIT
+    # PREDICT
 
-  X = sm.add_constant(df.drop('y', axis=1))
-  y = df['y']
-  model = sm.OLS(y, X).fit()
+    predicted = []
 
-  # PREDICT
+    for i in range(0, need_predict):
 
-  predicted = []
+        if i >= num_features:
+            p = [1.0] + predicted[-num_features:]
+        else:
+            p = [1.0] + list(cases_all["Russia"][field_name].iloc[-num_features + i:].values) + predicted
 
-  for i in range(0, need_predict):
+        for c, info in top_3_countries:
+            from_ind = -num_features + info["lag"] + i
+            to_ind = info["lag"] + i
+            p += list(cases_all[c][field_name].iloc[from_ind: to_ind])
 
-    if i >= num_features:
-      p = [1.0] + predicted[-num_features:]
-    else:
-      p = [1.0] + list(cases_all["Russia"][field_name].iloc[-num_features + i : ].values) + predicted
+        predicted.append(model.predict(p)[0])
 
-    for c, info in top_3_countries:
+    return np.array(predicted)
 
-      from_ind = -num_features + info["lag"] + i
-      to_ind = info["lag"] + i
-      p += list(cases_all[c][field_name].iloc[from_ind : to_ind])
-      
-    predicted.append(model.predict(p)[0])
-  
-  return np.array(predicted)
+
 # ///////////////////////////////////////////////////////////////////////
 # =======================================================================
 
@@ -172,11 +176,10 @@ def get_predict_linear(country, lags, need_predict, cases_all, use_filter=True, 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 def hwes_predict(data, country, days):
- 
     # create time series for country
     df = data[data['location'] == country]
     ts = df.set_index(pd.DatetimeIndex(df['date'])).asfreq('D').total_cases
-  
+
     # split the time series
     train = ts.iloc[:-days]
 
@@ -187,6 +190,8 @@ def hwes_predict(data, country, days):
     predict = fitted.forecast(days).values
 
     return predict
+
+
 # ///////////////////////////////////////////////////////////////////////
 # =======================================================================
 
